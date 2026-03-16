9523b28a8e28085a1cd775a48a68e27ebc402403
rodrigo <kakakpegado@hotmail.com>
2026-03-15 22:12:02 -0300

    feat: configura filas do RabbitMQ para análise de crédito e notificações

---
+++ Proposta-App/src/main/java/com/RodSolution/Proposta/App/config/RabbitMQConfiguration.java
@@ -0,0 +1,35 @@
+package com.RodSolution.Proposta.App.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta() {
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsnotificacao() {
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+}
